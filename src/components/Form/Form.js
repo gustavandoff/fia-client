e2367a08f76751d0913145ac31657edf73d6d7d6
Gustav <gustav.andoff@gmail.com>
2022-03-21 02:41:20 +0100

    Remove unnecessary code

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -8,33 +8,29 @@ const Form = ({ children, title, linkPath, linkText }) => {
     }
 
     return (
-        <div>
-            <section className="vh-100 gradient-custom">
-                <div className="container py-5 h-100">
-                    <div className="row d-flex justify-content-center align-items-center h-100">
-                        <div className="col-12 col-md-8 col-lg-6 col-xl-5">
-                            <div className="card bg-col-primary text-col-primary" style={{ borderRadius: '2rem' }}>
-                                <div className="card-body p-5 text-center">
+        <section className="vh-100 gradient-custom">
+            <div className="container py-5 h-100">
+                <div className="row d-flex justify-content-center align-items-center h-100">
+                    <div className="col-12 col-md-8 col-lg-6 col-xl-5">
+                        <div className="card bg-col-primary text-col-primary" style={{ borderRadius: '2rem' }}>
+                            <div className="card-body p-5 text-center">
+                                <div className="mb-md-5 mt-md-4 pb-2">
 
-                                    <div className="mb-md-5 mt-md-4 pb-2">
+                                    <h2 className="fw-bold mb-5 text-uppercase">{title ? title : ''}</h2>
 
-                                        <h2 className="fw-bold mb-5 text-uppercase">{title ? title : ''}</h2>
+                                    {children}
 
-                                        {children}
+                                </div>
 
-                                    </div>
-
-                                    <div>
-                                        {bottomLink}
-                                    </div>
-
+                                <div>
+                                    {bottomLink}
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
-            </section>
-        </div>
+            </div>
+        </section>
     );
 }
 
