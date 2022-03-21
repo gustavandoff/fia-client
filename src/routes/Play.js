2e3b887ca5a6070cd9717f349f92a75e2be37104
Gustav <gustav.andoff@gmail.com>
2022-03-21 17:56:03 +0100

    Add guest account warning

--- src/routes/Play.js
+++ src/routes/Play.js
@@ -7,14 +7,33 @@ import Navbar from "../components/Header/Navbar";
 const Play = ({ currentUser, setCurrentUser }) => {
     const [modal, setModal] = useState(null);
     const exampleModal = useRef();
-
     const navigate = useNavigate();
+    let linkPath;
 
     useEffect(() => {
-        setModal(
-            new Modal(exampleModal.current)
-        );
+        if (!currentUser) {
+            setModal(
+                new Modal(exampleModal.current)
+            );
+        }
+
     }, []);
+
+    const linkClicked = (path) => {
+        if (currentUser) return navigate(path);
+        linkPath = path;
+        modal.show();
+    }
+
+    const closeModal = () => {
+        modal.hide();
+        return navigate(linkPath);
+    }
+
+    const linkLogin = () => {
+        modal.hide();
+        return navigate('/login');
+    }
 
     let guestMessage = '';
     if (!currentUser) {
@@ -30,25 +49,14 @@ const Play = ({ currentUser, setCurrentUser }) => {
                             Du är inte inloggad och spelar därför som gäst. Dina spel kommer inte att sparas
                         </div>
                         <div className="modal-footer">
-                            <button type="button" className="btn btn-primary" onClick={() => modal.hide()}>Stäng</button>
+                            <button type="button" className="btn btn-primary" onClick={linkLogin}>Logga in</button>
+                            <button type="button" className="btn btn-primary" onClick={closeModal}>Fortsätt ändå</button>
                         </div>
                     </div>
                 </div>
             </div>
         );
     }
-
-
-    const linkCreateGame = () => {
-        modal.show();
-        //return navigate("/creategame");
-    }
-
-    const linkJoinGame = () => {
-        modal.show();
-        //return navigate("/joingame");
-    }
-
 
     return (
         <div>
@@ -57,17 +65,14 @@ const Play = ({ currentUser, setCurrentUser }) => {
 
 
             <div className="container py-5 my-5 h-100">
-                <button type="button" onClick={() => modal.show()} className="btn btn-primary">
-                    Launch demo modal
-                </button>
                 {guestMessage}
                 <div className="row d-flex justify-content-center align-items-center h-50">
                     <div className="col-12 col-md-8 col-lg-6 col-xl-5">
-                        <a onClick={linkCreateGame} className='card text-center btn btn-primary bg-col-primary stretched font-size-2 text-nowrap py-5 m-2' style={{ borderRadius: '2rem' }}>Skapa nytt spel</a>
+                        <a onClick={() => linkClicked('/creategame')} className='card text-center btn btn-primary bg-col-primary stretched font-size-2 text-nowrap py-5 m-2' style={{ borderRadius: '2rem' }}>Skapa nytt spel</a>
                     </div>
 
                     <div className="col-12 col-md-8 col-lg-6 col-xl-5">
-                        <a onClick={linkJoinGame} className='card text-center btn btn-primary bg-col-primary stretched font-size-2 text-nowrap py-5 m-2' style={{ borderRadius: '2rem' }}>Gå med i spel</a>
+                        <a onClick={() => linkClicked('/joingame')} className='card text-center btn btn-primary bg-col-primary stretched font-size-2 text-nowrap py-5 m-2' style={{ borderRadius: '2rem' }}>Gå med i spel</a>
                     </div>
                 </div>
             </div>
