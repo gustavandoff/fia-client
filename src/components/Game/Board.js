58acd15aab7170957a55bf9fa9bcde4dc62ff94c
Gustav <gustav.andoff@gmail.com>
2022-04-01 03:18:35 +0200

    Make nonplayer board arm grey

--- src/components/Game/Board.js
+++ src/components/Game/Board.js
@@ -7,6 +7,7 @@ const Board = ({ playerCount, circleSize, game, setSelectedPiece, selectedPiece,
     let v = (Math.PI / 180) * (360 / playerCount); // vinkeln mellan varje arm i radianer
     let d = circleSize;
     const players = game.players;
+    const boardArmCount = Object.keys(players).length > 4 ? Object.keys(players).length : 4;
 
     if (playerCount <= 4) {
         distanceFromCenter = (circleSize / 2);
@@ -17,13 +18,13 @@ const Board = ({ playerCount, circleSize, game, setSelectedPiece, selectedPiece,
     let rotDeg = 360 / playerCount;
     let render = [];
 
-    Object.keys(players).forEach((u, i) => {
-
-        const color = players[u] ? players[u].color : 'grey';
+    for (let i = 0; i < boardArmCount; i++) {
+        const thisPlayer = players[Object.keys(players).find(u => players[u].playerNumber - 1 === i)]
+        const color = thisPlayer ? thisPlayer.color : 'grey';
 
         render.push(<BoardArm key={i} distanceFromCenter={distanceFromCenter} movePieceToPos={movePieceToPos} moveIndicator={moveIndicator} setMoveIndicator={setMoveIndicator} selectedPiece={selectedPiece} setSelectedPiece={setSelectedPiece} game={game} currentUser={currentUser} color={color} degree={i * rotDeg} circleSize={circleSize} playerCount={playerCount} armNumber={i + 1} />);
 
-    });
+    }
 
     render.push(<FinishStepCircle key={-1} distanceFromCenter={distanceFromCenter} movePieceToPos={movePieceToPos} moveIndicator={moveIndicator} setMoveIndicator={setMoveIndicator} selectedPiece={selectedPiece} setSelectedPiece={setSelectedPiece} game={game} currentUser={currentUser} size={circleSize} />);
 
