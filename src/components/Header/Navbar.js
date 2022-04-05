b3a62607cfd33d302e1e1d4797e13afb3f82f9e1
Gustav Andoff <gustav.andoff@gmail.com>
2022-04-05 13:19:30 +0200

    Remove unused code

--- src/components/Header/Navbar.js
+++ src/components/Header/Navbar.js
@@ -1,14 +1,12 @@
 import '../../App.css';
 import NavRight from './NavRight';
 import { Link } from 'react-router-dom';
-import { MdHome } from "react-icons/md";
 
 const Navbar = ({ currentUser, setCurrentUser }) => {
     return (
         <header className="container-sm w-100 start-0 end-0 top-0 position-fixed">
             <nav className="container-xxl">
-                {//<button className="navbar-toggler collapsed" type="button" data-bs-toggle="collapse" data-bs-toggle="#bdNavbar"></button>
-                }<div className="row">
+                <div className="row">
 
                     <div className="col-1 col-sm-2 col-md-3 col-lg-4 col-xl-4"></div>
                     <div className="col-10 col-sm-8 col-md-6 col-lg-4 col-xl-4 text-center fia-border-bottom">
