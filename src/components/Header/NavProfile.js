5b1207d478f6b77918f32acc6a4f2fb338d19d40
Gustav Andoff <gustav.andoff@gmail.com>
2022-04-05 13:20:23 +0200

    Change css Get my games on button press

--- src/components/Header/NavProfile.js
+++ src/components/Header/NavProfile.js
@@ -6,7 +6,8 @@ import { useNavigate } from "react-router-dom";
 import axios from 'axios';
 
 const NavProfile = ({ currentUser, setCurrentUser }) => {
-    const [open, setOpen] = useState(false);
+    const [openDd, setOpenDd] = useState(false);
+    const [openMyGames, setOpenMyGames] = useState(false);
     const navigate = useNavigate();
 
     const logOut = () => {
@@ -23,32 +24,62 @@ const NavProfile = ({ currentUser, setCurrentUser }) => {
         return navigate('/login');
     }
 
+    const GameList = () => {
+        let result = [];
+        let gameList;
+
+        axios.get(`http://localhost:4000/gamesUser/${currentUser.username}/`)
+            .then(res => {
+                gameList = res.data;
+            })
+            .catch(error => {
+                console.error(error);
+            });
+
+        if (openMyGames) {
+            result.push(
+                <div key={0} onClick={() => setOpenMyGames(false)}>
+                    Mina spel:
+                </div>
+            );
+        } else {
+            result.push(
+                <div key={2} onClick={() => setOpenMyGames(true)}>
+                    Lista mina spel
+                </div>
+            );
+        }
+
+
+
+        return result;
+    }
+
     const DropDown = () => {
         let dropDown = '';
 
         const LogOutIn = () => {
+            const aClassName = "text-col-primary text-center text-decoration-none text-white-50 ms-1";
             if (currentUser.username.startsWith('gäst')) {
                 return (
-                    <a onClick={logIn} className="text-col-primary">
+                    <a onClick={logIn} className={aClassName}>
                         Logga in
                     </a>
                 );
             } else {
                 return (
-                    <a onClick={logOut} className="text-col-primary">
+                    <a onClick={logOut} className={aClassName}>
                         Logga ut
                     </a>
                 );
             }
         }
 
-        if (open) {
+        if (openDd) {
             dropDown = (
                 <DropdownMenu>
                     <DropdownItem>
-                        <div>
-                            Lista mina spel (Fungerar inte)
-                        </div>
+                        <GameList />
                     </DropdownItem>
 
                     <DropdownItem>
@@ -61,15 +92,19 @@ const NavProfile = ({ currentUser, setCurrentUser }) => {
         return dropDown;
     }
 
-
-
     return (
-        <div onClick={() => setOpen(!open)} className="btn text-col-primary p-0 fia-dropdown-head">
-            <span className="text-col-secondary font-size-1-5 fw-bold">{currentUser.username}</span>
-            <MdKeyboardArrowDown className="text-col-secondary font-size-1-5" style={{
-                transform: `rotate(${open ? 180 : 0}deg)`,
-                transition: 'transform 300ms'
-            }} />
+        <div className="btn text-col-primary p-0 fia-dropdown-head">
+            <div onClick={() => {
+                setOpenMyGames(false);
+                setOpenDd(!openDd);
+            }}>
+                <span className="text-col-secondary font-size-1-5 fw-bold">{currentUser.username}</span>
+                <MdKeyboardArrowDown className="text-col-secondary font-size-1-5" style={{
+                    transform: `rotate(${openDd ? 180 : 0}deg)`,
+                    transition: 'transform 150ms'
+                }} />
+            </div>
+
             <DropDown />
 
         </div>
