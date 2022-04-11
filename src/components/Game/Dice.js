d45a8592211dff7222c01f4ef7293e249fabdbfa
Gustav <gustav.andoff@gmail.com>
2022-04-11 13:59:20 +0200

    Show dice in center, move to corner when clicked

--- src/components/Game/Dice.js
+++ src/components/Game/Dice.js
@@ -1,67 +1,76 @@
 import { useState, useEffect } from "react";
 import axios from 'axios';
 
-const Dice = ({ currentDiceRoll, setCurrentDiceRoll, socket, game }) => {
+const Dice = ({ windowDimensions, currentDiceRoll, setCurrentDiceRoll, socket, game }) => {
     const [showedRoll, setShowedRoll] = useState(1);
+    const [isClicked, setIsClicked] = useState(false);
+
+    let showRollTimeOut;
 
     useEffect(() => {
         let animationInterval;
-        if (!currentDiceRoll && !animationInterval) {
-            animationInterval = setInterval(() => {
-                setShowedRoll((oldRoll) => oldRoll === 6 ? 1 : oldRoll + 1);
-            }, 400);
-        } else {
-            clearInterval(animationInterval);
-            setShowedRoll(currentDiceRoll);
+        if (!currentDiceRoll) {
+            if (isClicked) {
+                console.log('--------------------------------------------------');
+                console.log('showRollTimeOut:', showRollTimeOut);
+                clearInterval(animationInterval);
+                showRollTimeOut = setTimeout(async () => {
+                    setCurrentDiceRoll(showedRoll);
+                    await socket.emit('updateGameDiceRoll', {
+                        game,
+                        newDiceRoll: showedRoll,
+                    });
+                }, 1000);
+
+                console.log('showRollTimeOut 2:', showRollTimeOut);
+            } else if (!animationInterval) {
+                animationInterval = setInterval(() => {
+                    setShowedRoll((oldRoll) => oldRoll === 6 ? 1 : oldRoll + 1);
+                }, 1000);
+            }
         }
+
 
         return () => {
             clearInterval(animationInterval);
+            clearTimeout(showRollTimeOut);
         }
-    }, [currentDiceRoll]);
+    }, [isClicked]);
 
-    const Side = ({ number }) => {
-
-
-
-        const boxShadow = !currentDiceRoll ? `0 0 1rem 1rem white` : '';
-        const cursor = !currentDiceRoll ? 'pointer' : 'default';
-
-        const src = require(`../../assets/images/dice/${number ? number : showedRoll}.jpg`);
-
-        return (
-            <img className='cursor-pointer' src={src} alt={'tärning som visar' + number ? number : showedRoll} style={{
-                width: '7rem',
-                height: '7rem',
-                boxShadow: boxShadow,
-                cursor: cursor,
-            }} />
-        )
-    }
-
-    const diceClicked = () => {
+    const diceClicked = async () => {
         console.log('diceClicked currentDiceRoll:', currentDiceRoll);
         if (!currentDiceRoll) {
-            rollDice();
+            rollDice()
+                .then(async diceRoll => {
+                    console.log('Rolled ', diceRoll);
+                    setShowedRoll(diceRoll);
+                    setIsClicked(true);
+                });
         }
     }
 
     const rollDice = async () => {
         console.log('Rolling dice');
-        axios.get(`http://${window.location.hostname}:4000/dice`)
+        return axios.get(`http://${window.location.hostname}:4000/dice`)
             .then(async res => {
-                setCurrentDiceRoll(res.data);
-                await socket.emit('updateGameDiceRoll', {
-                    game,
-                    newDiceRoll: res.data,
-                });
+                console.log('axios roll dice res.data:', res.data);
+                const data = res.data;
+                return data;
             });
     }
 
+    const boxShadow = !currentDiceRoll && !isClicked ? `0 0 1rem 1rem white` : '';
+    const cursor = !currentDiceRoll && !isClicked ? 'pointer' : 'default';
+
+    const src = require(`../../assets/images/dice/${currentDiceRoll ? currentDiceRoll : showedRoll}.jpg`);
+
     return (
-        <div onClick={diceClicked}>
-            <Side number={currentDiceRoll} />
-        </div>
+        <img onPointerDown={diceClicked} className={`${!currentDiceRoll ? 'position-relative' : 'position-fixed end-0 bottom-0'} pe-auto`} src={src} alt={'tärning som visar ' + (currentDiceRoll ? currentDiceRoll : showedRoll)} style={{
+            boxShadow,
+            cursor,
+            transform: `translateY(${!currentDiceRoll ? windowDimensions.height * 0.3 : 0}px) scale(${!currentDiceRoll ? 1.5 : 0.8})`,
+        }} />
+
     )
 }
 
