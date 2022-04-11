d6a0e7b202447a173cea6a16104d2f77e0400737
Gustav <gustav.andoff@gmail.com>
2022-04-11 18:40:19 +0200

    Add margin top

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -9,7 +9,7 @@ const Form = ({ children, title, linkPath, linkText }) => {
 
     return (
         <section className="vh-100 gradient-custom">
-            <div className="container py-5 h-100">
+            <div className="container mt-5 py-5 h-100">
                 <div className="row d-flex justify-content-center align-items-center h-100">
                     <div className="col-12 col-md-8 col-lg-6 col-xl-5">
                         <div className="card bg-col-primary text-col-primary" style={{ borderRadius: '2rem' }}>
