06ad5a0d3d1d5836694bd95f3af7cee47c177e53
Gustav <gustav.andoff@gmail.com>
2022-04-11 17:14:07 +0200

    Add comments Fix pointerEvent bug

--- src/components/Game/Dice.js
+++ src/components/Game/Dice.js
@@ -14,7 +14,7 @@ const Dice = ({ windowDimensions, currentDiceRoll, setCurrentDiceRoll, socket, g
                 console.log('--------------------------------------------------');
                 console.log('showRollTimeOut:', showRollTimeOut);
                 clearInterval(animationInterval);
-                showRollTimeOut = setTimeout(async () => {
+                showRollTimeOut = setTimeout(async () => { // tärningskastet ska visas i mitten av skärmen i en sekund innan den flyttas till hörnet
                     setCurrentDiceRoll(showedRoll);
                     await socket.emit('updateGameDiceRoll', {
                         game,
@@ -24,7 +24,7 @@ const Dice = ({ windowDimensions, currentDiceRoll, setCurrentDiceRoll, socket, g
 
                 console.log('showRollTimeOut 2:', showRollTimeOut);
             } else if (!animationInterval) {
-                animationInterval = setInterval(() => {
+                animationInterval = setInterval(() => { // varje sekund kommer tärningen växla siffra innan man tryckt på den
                     setShowedRoll((oldRoll) => oldRoll === 6 ? 1 : oldRoll + 1);
                 }, 1000);
             }
@@ -41,7 +41,7 @@ const Dice = ({ windowDimensions, currentDiceRoll, setCurrentDiceRoll, socket, g
         console.log('diceClicked currentDiceRoll:', currentDiceRoll);
         if (!currentDiceRoll) {
             rollDice()
-                .then(async diceRoll => {
+                .then(async diceRoll => { // rollDice returnerar en promise med träningskastet som körs här
                     console.log('Rolled ', diceRoll);
                     setShowedRoll(diceRoll);
                     setIsClicked(true);
@@ -51,23 +51,24 @@ const Dice = ({ windowDimensions, currentDiceRoll, setCurrentDiceRoll, socket, g
 
     const rollDice = async () => {
         console.log('Rolling dice');
-        return axios.get(`http://${window.location.hostname}:4000/dice`)
+        return axios.get(`http://${window.location.hostname}:4000/dice`) // skickar get-request för att få tärningskastet och returnerar promisen
             .then(async res => {
                 console.log('axios roll dice res.data:', res.data);
-                const data = res.data;
-                return data;
+                return res.data;
             });
     }
 
     const boxShadow = !currentDiceRoll && !isClicked ? `0 0 1rem 1rem white` : '';
     const cursor = !currentDiceRoll && !isClicked ? 'pointer' : 'default';
+    const pointerEvents = !currentDiceRoll && !isClicked ? 'auto' : 'none';
 
     const src = require(`../../assets/images/dice/${currentDiceRoll ? currentDiceRoll : showedRoll}.jpg`);
 
     return (
-        <img onPointerDown={diceClicked} className={`${!currentDiceRoll ? 'position-relative' : 'position-fixed end-0 bottom-0'} pe-auto`} src={src} alt={'tärning som visar ' + (currentDiceRoll ? currentDiceRoll : showedRoll)} style={{
+        <img onPointerDown={diceClicked} className={`${!currentDiceRoll ? 'position-relative' : 'position-fixed end-0 bottom-0'} `} src={src} alt={'tärning som visar ' + (currentDiceRoll ? currentDiceRoll : showedRoll)} style={{
             boxShadow,
             cursor,
+            pointerEvents,
             transform: `translateY(${!currentDiceRoll ? windowDimensions.height * 0.3 : 0}px) scale(${!currentDiceRoll ? 1.5 : 0.8})`,
         }} />
 
